6b3748c59715048711729b49272693e8d5c47058
黄显东 <xiandong.huang@hand-china.com>
2018-09-29 20:41:51 +0800

    [IMP] add check MySql funtion and create file funtion

--- pkg/server/type.go
+++ pkg/server/type.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"time"
 	"fmt"
+	"database/sql"
+	"os/exec"
 )
 
 type PortRequest struct {
@@ -25,7 +27,61 @@ type StorageStatus struct {
 	Success bool `json:"success"`
 	Free string  `json:"free"`
 }
+type MysqlInfo struct {
+	MysqlHost string `json:"mysql_host"`
+	MysqlPort string `json:"mysql_port"`
+	MysqlName string `json:"mysql_name"`
+	MysqlPwd  string `json:"mysql_pwd"`
+}
 
+type Requst struct {
+	Scop string 		`json:"scop"`
+	Mysql MysqlInfo 	`json:"mysql_info"`
+	DatabaseName string	`json:"database_name"`
+	SQL  string 		`json:"sql"`
+}
+
+type CommondExec struct {
+	CommondLine string `json:"commond"`
+}
+
+func (C *CommondExec)ExecuteCommond() (err error)  {
+	cmd := exec.Command("bash", "-c", C.CommondLine)
+	err = cmd.Run()
+	if err != nil {
+		log.Error(err.Error())
+	}
+	return
+}
+
+func (M *MysqlInfo)ConnetMySql() (db *sql.DB, err error)  {
+	ConnetInfo   := fmt.Sprint( M.MysqlName,":",M.MysqlPwd,"@tcp(",M.MysqlHost,":",M.MysqlPort,")/?charset=utf8&timeout=3s")
+	db, err = sql.Open("mysql", ConnetInfo)
+	if err != nil {
+		log.Errorf("Failed to connect mysql: %s", err)
+		return
+	}
+	err = db.Ping()
+	if err != nil {
+		log.Errorf("Failed to ping mysql: %s", err)
+	}
+	return
+}
+func (R *Requst)CreateData(db *sql.DB) (err error) {
+	if R.Scop == "database" {
+		_,err = db.Exec(R.SQL)
+		if err != nil {
+			log.Errorf("Failed to create database: %s", err)
+		}
+	} else {
+		_,err = db.Exec(fmt.Sprint("USE ",R.DatabaseName))
+		_,err = db.Exec(R.SQL)
+		if err != nil {
+			log.Errorf("Failed to create table: %s", err)
+		}
+	}
+	return
+}
 
 func (s *ServerAddr) StartNetCheck() (err error){
 	c := &http.Client{
